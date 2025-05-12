32e9472b1f25afe9de6f772aa81aabb7043c4198
SirLazubone <a.berezutsky@mail.ru>
2025-05-12 21:41:08 +0300

    fix api

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from bot.handlers.payment import router as payment_router
 
 app = FastAPI(title="VPN Bot API")
+app.include_router(payment_router, prefix="/webhook")
 
 # Подключаем роутер для платежей
 # app.include_router(payment_router)
